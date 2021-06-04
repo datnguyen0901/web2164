2ead4c4cf48df4bae150081f640ced4163b8a673
datnguyen0901 <84694940+datnguyen0901@users.noreply.github.com>
2021-06-04 10:33:04 +0700

    Add files via upload

--- 02.03.01_index.js
+++ 02.03.01_index.js
@@ -4,6 +4,7 @@
 // Mở thư viện fs, express
 // const fs = require("fs");
 const express = require("express");
+var path = require('path');
 // Tham số - Web lắng nghe tại xport
 xport = process.env.PORT || 3000;
 // Tạo App Web
@@ -15,28 +16,28 @@ const router = express.Router();
 //--- Home
 router.get('/',
     (req, res) => {
-        res.sendFile("./view/home.html");
+        res.sendFile(__dirname + "/view/home.html");
     }
 );
 
 //--- Profile
 router.get('/profile',
     (req, res) => {
-        res.sendFile("./view/profile.html");
+        res.sendFile(__dirname + "/view/profile.html");
     }
 );
 
 //--- Login
 router.get('/login',
     (req, res) => {
-        res.sendFile("./view/login.html");
+        res.sendFile(__dirname + "/view/login.html");
     }
 );
 
 //--- Logout
 router.get('/logout',
     (req, res) => {
-        res.sendFile("./view/logout.html");
+        res.sendFile(__dirname + "/view/logout.html");
     }
 );
 
